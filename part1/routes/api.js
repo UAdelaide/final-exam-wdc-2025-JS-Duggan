03ab3ec077f03a612150bd9d3e7d013d94cec836
Jack Duggan <jacksdggn@gmail.com>
2025-06-20 02:20:34 +0000

    [automatic commit] 2025-06-20 02:20:34 UTC 220.245.155.98

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -3,41 +3,40 @@ var router = express.Router();
 var mysql = require('mysql2/promise');
 
 (async () => {
+  try {
+      const connection = await mysql.createConnection({
+        socketPath: '/var/run/mysqld/mysqld.sock'
+      });
 
-})
-try {
-    const connection = await mysql.createConnection({
-      socketPath: '/var/run/mysqld/mysqld.sock'
-    });
+      await connection.query('CREATE DATABASE IF NOT EXISTS DogWalkService');
+      await connection.end();
 
-    await connection.query('CREATE DATABASE IF NOT EXISTS DogWalkService');
-    await connection.end();
+      const db = await mysql.createConnection({
+        socketPath: '/var/run/mysqld/mysqld.sock',
+        database: 'DogWalkService'
+      });
 
-    const db = await mysql.createConnection({
-      socketPath: '/var/run/mysqld/mysqld.sock',
-      database: 'DogWalkService'
-    });
-
-    await db.execute(`
-      CREATE TABLE IF NOT EXISTS Dogs (
-        dog_id INT AUTO_INCREMENT PRIMARY KEY,
-        owner_id INT NOT NULL,
-        name VARCHAR(50) NOT NULL,
-        size ENUM('small', 'medium', 'large') NOT NULL,
-        FOREIGN KEY (owner_id) REFERENCES Users(user_id)
-      );
-    `);
-    await connection.execute(`
-      INSERT INTO Dogs (name, size, owner_id) VALUES
-      ('Jack', 'large', (SELECT user_id FROM Users WHERE username = 'alice123')),
-    `);
-    await connection.end();
+      await db.execute(`
+        CREATE TABLE IF NOT EXISTS Dogs (
+          dog_id INT AUTO_INCREMENT PRIMARY KEY,
+          owner_id INT NOT NULL,
+          name VARCHAR(50) NOT NULL,
+          size ENUM('small', 'medium', 'large') NOT NULL,
+          FOREIGN KEY (owner_id) REFERENCES Users(user_id)
+        );
+      `);
+      await connection.execute(`
+        INSERT INTO Dogs (name, size, owner_id) VALUES
+        ('Jack', 'large', (SELECT user_id FROM Users WHERE username = 'alice123')),
+      `);
+      await connection.end();
 
 
-  } catch (error) {
-    res.status(400).send();
-  }
+    } catch (error) {
+      res.status(400).send();
+    }
 });
+
 
 router.get('/dogs', async function(req, res, next) {
 
