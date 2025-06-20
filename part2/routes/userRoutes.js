ecfa615d1e1c13194d6cc38e93a28d3c6da2a514
Jack Duggan <jacksdggn@gmail.com>
2025-06-20 06:35:13 +0000

    [automatic commit] 2025-06-20 06:35:13 UTC 220.245.155.98

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -49,8 +49,8 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
     // re-route user to dashboard page
-    if (rows[0].role == 'owner') res.redirect('/owner-dashboard.html');
-    if ()
+    if (rows[0].role === 'owner') res.redirect('/owner-dashboard.html');
+    if (rows[0].role === 'walker') res.redirect('/walker-dashboard.html');
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
