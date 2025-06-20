4e11969255c90cbe288dbb9941cfa68c6defa2e7
Jack Duggan <jacksdggn@gmail.com>
2025-06-20 02:10:41 +0000

    [automatic commit] 2025-06-20 02:10:41 UTC 220.245.155.98

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -6,11 +6,14 @@ var mysql = require('mysql2/promise');
 router.get('/dogs', async function(req, res, next) {
   try {
     const connection = await mysql.createConnection({
-      socketPath: '/var/run/mysqld/mysqld.sock'
+      socketPath: '/var/run/mysqld/mysqld.sock',
+      database
     });
 
-    await connection.query('CREATE DATABASE IF NOT EXISTS testdb');
+    await connection.query('CREATE DATABASE IF NOT EXISTS DogWalkService');
     await connection.end();
+
+
   } catch (error) {
     res.status(400).send();
   }
