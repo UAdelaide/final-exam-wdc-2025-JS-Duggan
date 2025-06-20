bfc6f0cd611f07484a51da180b928237b214608e
Jack Duggan <jacksdggn@gmail.com>
2025-06-20 02:03:53 +0000

    [automatic commit] 2025-06-20 02:03:52 UTC 220.245.155.98

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -3,8 +3,12 @@ var router = express.Router();
 var mysql = require('mysql2/promise');
 
 
-router.get('/dogs', function(req, res, next) {
+router.get('/dogs', async function(req, res, next) {
   try {
+    connection = await mysql.createConnection({
+    socketPath: '/var/run/mysqld/mysqld.sock',
+    database: 'behind_the_beat'
+    });
     let results;
     res.status(200).send(results);
   } catch (error) {
