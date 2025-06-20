914f1f2261d0bdd5963965b792a9812826170332
Jack Duggan <jacksdggn@gmail.com>
2025-06-20 07:15:45 +0000

    [automatic commit] 2025-06-20 07:15:45 UTC 220.245.155.98

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -62,7 +62,9 @@ router.post('/login', async (req, res) => {
 router.post('/logout', (req, res) => {
   // clear session, cookies and
   // redirect user to login page
-  
+  req.session.destroy(err => {
+    if (err)
+  })
 });
 
 module.exports = router;
