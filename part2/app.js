2981cfe50efce85e2c38c722e6bc76a8dbe280a1
Jack Duggan <jacksdggn@gmail.com>
2025-06-20 06:46:48 +0000

    [automatic commit] 2025-06-20 06:46:48 UTC 220.245.155.98

--- part2/app.js
+++ part2/app.js
@@ -15,7 +15,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    
+    maxAge: 1000 * 60 * 60 * 24 // 24 hours
   }
 }));
 app.use(express.static(path.join(__dirname, '/public')));
