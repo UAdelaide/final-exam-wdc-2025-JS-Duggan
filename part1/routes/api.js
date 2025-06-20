a2cdc65bc1cd8d178e34d677a508c4fb78d0ff3b
Jack Duggan <jacksdggn@gmail.com>
2025-06-20 02:58:55 +0000

    [automatic commit] 2025-06-20 02:58:55 UTC 220.245.155.98

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -176,7 +176,11 @@ router.get('/walkers/summary', async function(req, res, next) {
         Users
         JOIN WalkRatings on Users.user_id = WalkRatings.walker_id
         JOIN WalkRequests on WalkRequests.request_id = WalkRatings.request_id AND WalkRatings.stauts = 'completed'
-    `)
+      WHERE
+        Users.role = 'walker'
+      GROUP BY
+        Users.user_id;
+        `)
     res.status(200).send(results);
   } catch (error) {
     res.status(400).send('Error: ' + error);
