65cc2af9a981449529d83306b58c930c442fb010
Jack Duggan <jacksdggn@gmail.com>
2025-06-20 08:01:51 +0000

    [automatic commit] 2025-06-20 08:01:51 UTC 220.245.155.98

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -29,10 +29,11 @@ router.post('/register', async (req, res) => {
 });
 
 router.get('/me', (req, res) => {
-  if (!req.session.user) {
+  // change to 
+  if (!req.session.user_id) {
     return res.status(401).json({ error: 'Not logged in' });
   }
-  res.json(req.session.user);
+  res.json(req.session.user_id);
 });
 
 // Post login (modified)
