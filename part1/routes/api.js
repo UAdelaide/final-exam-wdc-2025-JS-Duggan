5a3d4922b4d819fbcba4fe04c5951059aca48ebd
Jack Duggan <jacksdggn@gmail.com>
2025-06-20 02:00:48 +0000

    [automatic commit] 2025-06-20 02:00:48 UTC 220.245.155.98

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -2,15 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/dogs', function(req, res, next) {
+  try {
 
+  } catch (error) {
+
+  }
 });
 
 router.get('/walkrequests/open', function(req, res, next) {
+  try {
 
+  } catch (error) {
+
+  }
 });
 
 router.get('/walkers/summary', function(req, res, next) {
+  try {
 
+  } catch (error) {
+
+  }
 });
 
 module.exports = router;
